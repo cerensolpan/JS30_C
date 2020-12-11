a548bdbfdc1ffdec2e92fb0e42458cd98226a28c
cerensolpan <cerensolpan@gmail.com>
2020-12-11 15:13:43 +0300

    Fix The Error

--- 03_Playing with CSS Variables and JS/playing.js
+++ 03_Playing with CSS Variables and JS/playing.js
@@ -1,9 +1,10 @@
 const inputs = document.querySelectorAll('.controls input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function handleUpdate() {
+  const suffix =  this.dataset.sizing || '';
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
